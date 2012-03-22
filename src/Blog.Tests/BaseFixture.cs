9fa29cf676375f21d0d440849f2e8bc195a326d3
Leonardo Cardoso <leonardo@buzzero.com>
2012-03-22 19:02:22 -0300

    Adding app code.

---
+++ src/Blog.Tests/BaseFixture.cs
@@ -0,0 +1,56 @@
+using System;
+using NUnit.Framework;
+using log4net;
+using log4net.Config;
+
+namespace Blog.Tests
+{
+    public abstract class BaseFixture
+    {
+        protected static ILog log = new Func<ILog>(() =>
+                                                       {
+                                                           XmlConfigurator.Configure();
+                                                           return LogManager.GetLogger(typeof(BaseFixture));
+                                                       }).Invoke();
+
+        protected virtual void OnFixtureSetup()
+        {
+        }
+
+        protected virtual void OnFixtureTeardown()
+        {
+        }
+
+        protected virtual void OnSetup()
+        {
+        }
+
+        protected virtual void OnTeardown()
+        {
+        }
+
+        [TestFixtureSetUp]
+        public void FixtureSetup()
+        {
+            OnFixtureSetup();
+        }
+
+        [TestFixtureTearDown]
+        public void FixtureTeardown()
+        {
+            OnFixtureTeardown();
+        }
+
+        [SetUp]
+        public void Setup()
+        {
+            OnSetup();
+        }
+
+        [TearDown]
+        public void Teardown()
+        {
+            OnTeardown();
+        }
+    }
+}
